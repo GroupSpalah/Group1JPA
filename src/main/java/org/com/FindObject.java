7532f40f22dd770942cb93426174889f9e4e194e
Andre <valevskii.a@gmail.com>
2024-07-28 13:32:20 +0200

    Save and find objects via JPA

---
+++ src/main/java/org/com/FindObject.java
@@ -0,0 +1,35 @@
+package org.com;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import lombok.Cleanup;
+
+class FindObject {
+    public static void main(String[] args) {
+
+        @Cleanup
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("test-jpa");
+
+        @Cleanup
+        EntityManager em = factory.createEntityManager();
+
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+
+        Country country = em.find(Country.class, 1);
+
+        System.out.println(country);
+
+        transaction.commit();
+
+    }
+}
+
+/**
+ * 1 2 3 4
+ *
+ *
+ */
