0a1a2265c83472e5fb22fad6e569de59c27a1632
Andre <valevskii.a@gmail.com>
2024-07-28 13:58:19 +0200

    Move classes to lesson package

--- src/main/java/org/com/lessons/Country.java
+++ src/main/java/org/com/lessons/Country.java
@@ -1,4 +1,4 @@
-package org.com;
+package org.com.lessons;
 
 import jakarta.persistence.*;
 import lombok.*;
--- src/main/java/org/com/lessons/FindObject.java
+++ src/main/java/org/com/lessons/FindObject.java
@@ -1,4 +1,4 @@
-package org.com;
+package org.com.lessons;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
